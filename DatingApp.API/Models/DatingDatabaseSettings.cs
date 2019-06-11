511fcf254e0055536f26005f05c1df655e053575
Carlos Aurelio <carlosspok@gmail.com>
2019-06-10 22:27:49 -0300

    Iniciando a Aplicação DatingApp

---
+++ DatingApp.API/Models/DatingDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace DatingApp.API.Models
+{
+    public class DatingDatabaseSettings : IDatingDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IDatingDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
